c1929f73658630c63c931d8941617127613539f5
liaozan <378024053@qq.com>
2023-06-18 15:37:34 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+}
+
+nav a {
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    font-size: 1rem;
+    padding: 1rem 0;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index">
+    <h1>This is an index page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .index {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
